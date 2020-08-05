b58aa78709b263976b0e17085422e982ab6cca43
MauricioArgu <Mauro66Arg@gmail.com>
2020-08-05 17:01:48 -0600

    GetActives

--- src/jpa/controller/EmpController.java
+++ src/jpa/controller/EmpController.java
@@ -5,9 +5,12 @@
  */
 package jpa.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import jpa.connection.ConnectionFactory;
 import jpa.entity.RhEmpleado;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 /**
  *
@@ -19,6 +22,33 @@ public class EmpController extends AbstractController<RhEmpleado>
     @Override
     protected EntityManager getEntityManager() {
         return ConnectionFactory.getInstance().getEntityManagerFactory().createEntityManager();
+    }
+    
+    public List<RhEmpleado> findActive(RhEmpleado entity) throws Exception
+    {
+        EntityManager em = getEntityManager();
+        
+        
+        List<RhEmpleado> entities = new ArrayList<>();
+        
+        try
+        {
+            String jpql = "SELECT o FROM " + entity.getClass().getSimpleName() + " o WHERE EMP_ESTADO = 0";
+            Query q = em.createQuery(jpql);
+            entities = q.getResultList();
+        }
+        catch(Exception e)
+        {
+            throw new Exception(e);
+        }
+        finally
+        {
+            if (em.isOpen()) 
+            {
+                em.close();
+            }
+        }
+        return entities;
     }
 
     
