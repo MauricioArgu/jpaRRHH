1673acdfb0e6beec79022e1a0975ddd45b76c466
MauricioArgu <Mauro66Arg@gmail.com>
2020-08-06 14:38:19 -0600

    Login Controller

--- src/jpa/controller/UsuController.java
+++ src/jpa/controller/UsuController.java
@@ -8,6 +8,7 @@ package jpa.controller;
 import jpa.connection.ConnectionFactory;
 import jpa.entity.RhUsuario;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 /**
  *
@@ -15,6 +16,12 @@ import javax.persistence.EntityManager;
  */
 public class UsuController extends AbstractController<RhUsuario>
 {
+    public RhUsuario validarUsuario(String email)
+    {
+        Query q = getEntityManager().createQuery("SELECT u FROM RhUsuario u WHERE u.usCorreo = :email");
+        q.setParameter("email", email);
+        return(RhUsuario) q.getSingleResult();
+    }
 
     @Override
     protected EntityManager getEntityManager() {
